99ff47a1b5ecdb33de1971cfd0c20360169b5e96
Mehrshad Shams <a-meshams@expedia.com>
2018-02-20 19:39:13 -0500

    Add timing

--- train.py
+++ train.py
@@ -21,11 +21,19 @@ def main():
     non_vehicles = list(filter(lambda x: not x.startswith('.'), glob.glob('data/non-vehicles/**/*')))
 
     for cspace in COLOR_SPACES:
+        st = time.time()
+
         print(f'Traing on {cspace}')
+
+        print('Loading features ...')
         X = extract_features(vehicles + non_vehicles, cspace=cspace)
+        print('Features loaded in {}'.format(time.time() - st))
+
         y = np.concatenate([np.ones(len(vehicles)), np.zeros(len(non_vehicles))])
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+        print('Scaling features ...')
 
         scaler = StandardScaler()
         scaler.fit(X_train)
@@ -38,14 +46,20 @@ def main():
         joblib.dump({'X': X_train_scaled, 'y': y_train}, f'train{cspace}.pkl')
         joblib.dump({'X': X_test_scaled, 'y': y_test}, f'test{cspace}.pkl')
 
+        train_start = time.time()
+        print('Training ...')
+
         parameters = {'kernel': ('linear', 'rbf'), 'C': [1, 10]}
         svr = SVC()
         clf = GridSearchCV(svr, parameters)
         clf.fit(X_train_scaled, y_train)
 
+        print('Training finished in {}ms'.format(time.time() - train_start))
         print(clf.best_params_)
 
-        print('Score on test {}'.format(clf.score(X_test_scaled, y_test)))
+        score = clf.score(X_test_scaled, y_test)
+
+        print('Score on test {}, Time={}ms'.format(score, time.time() - st))
 
         joblib.dump(clf, f'svm{cspace}.pkl')
 
